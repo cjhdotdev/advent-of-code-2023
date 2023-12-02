09a9c0cf2fd179ddf8352ae6b84a6799739b4791
Chris Hogben <chris@cjh.dev>
2023-12-02 17:11:17 +0000

    Day two, part one

--- app/Commands/DayTwoCommand.php
+++ app/Commands/DayTwoCommand.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Commands;
 
+use Illuminate\Support\Str;
+
 class DayTwoCommand extends BaseCommand
 {
     protected $signature = 'day:02';
@@ -17,11 +19,52 @@ class DayTwoCommand extends BaseCommand
 
     public function solvePuzzlePartOne(): string
     {
-        return '';
+        return strval(
+            $this
+                ->getFileByLines()
+                ->filter()
+                ->mapWithKeys(fn ($line) => [
+                    Str::of($line)->matchAll('/Game ([0-9]+):/')->first() => Str::of($line)->matchAll('/: (.+)/')->first(),
+                ])
+                ->reduce(fn ($total, $gameStr, $gameId) => $total + ($this->allLooksValid($gameStr) ? intval($gameId) : 0), 0)
+        );
     }
 
     public function solvePuzzlePartTwo(): string
     {
         return '';
+    }
+
+    private function allLooksValid(string $gameString): bool
+    {
+        return Str::of($gameString)
+            ->explode('; ')
+            ->reject(fn ($game) => $this->isLookInvalid($game))
+            ->isEmpty();
+    }
+
+    private function isLookInvalid(string $look): bool
+    {
+        return Str::of($look)
+            ->explode(', ')
+            ->filter(fn ($cubes) => (
+                collect(array_combine(
+                    Str::of($cubes)->matchAll('/(red|green|blue)/')->toArray(),
+                    Str::of($cubes)->matchAll('/([0-9]+)/')->toArray(),
+                ))
+                    ->reject(fn ($count, $colour) => $this->hasTooManyForColour($colour, $count))
+                    ->isEmpty()
+            ))
+            ->isEmpty();
+    }
+
+    private function hasTooManyForColour(string $colour, string $count): bool
+    {
+        return match ($colour) {
+            'red' => intval($count) > 12,
+            'green' => intval($count) > 13,
+            'blue' => intval($count) > 14,
+            default => true,
+        };
     }
 }
