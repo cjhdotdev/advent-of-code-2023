dae6f1ff0db44a18acba82f256b8cb00315e1ba6
Chris Hogben <chris@cjh.dev>
2023-12-02 18:21:23 +0000

    Day two, part two

--- app/Commands/DayTwoCommand.php
+++ app/Commands/DayTwoCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Commands;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
 class DayTwoCommand extends BaseCommand
@@ -21,41 +22,74 @@ class DayTwoCommand extends BaseCommand
     {
         return strval(
             $this
-                ->getFileByLines()
-                ->filter()
-                ->mapWithKeys(fn ($line) => [
-                    Str::of($line)->matchAll('/Game ([0-9]+):/')->first() => Str::of($line)->matchAll('/: (.+)/')->first(),
-                ])
-                ->reduce(fn ($total, $gameStr, $gameId) => $total + ($this->allLooksValid($gameStr) ? intval($gameId) : 0), 0)
+                ->getGameCollection()
+                ->reduce(fn ($total, $gameStr, $gameId) => $total + ($this->allLooksValid($gameStr) ? intval($gameId) : 0))
         );
     }
 
     public function solvePuzzlePartTwo(): string
     {
-        return '';
+        return strval(
+            $this
+                ->getGameCollection()
+                ->reduce(fn ($total, $gameStr, $gameId) => $total + ($this->findPowerOfFewestCubes($gameStr)))
+        );
+    }
+
+    private function getGameCollection(): Collection
+    {
+        return $this
+            ->getFileByLines()
+            ->filter()
+            ->mapWithKeys(fn ($line) => [
+                Str::of($line)->matchAll('/Game ([0-9]+):/')->first() => Str::of($line)->matchAll('/: (.+)/')->first(),
+            ]);
     }
 
     private function allLooksValid(string $gameString): bool
     {
         return Str::of($gameString)
             ->explode('; ')
-            ->reject(fn ($game) => $this->isLookInvalid($game))
-            ->isEmpty();
-    }
-
-    private function isLookInvalid(string $look): bool
-    {
-        return Str::of($look)
-            ->explode(', ')
-            ->filter(fn ($cubes) => (
-                collect(array_combine(
-                    Str::of($cubes)->matchAll('/(red|green|blue)/')->toArray(),
-                    Str::of($cubes)->matchAll('/([0-9]+)/')->toArray(),
-                ))
-                    ->reject(fn ($count, $colour) => $this->hasTooManyForColour($colour, $count))
+            ->reject(fn ($game) => (
+                Str::of($game)
+                    ->explode(', ')
+                    ->filter(fn ($cubes) => (
+                        $this
+                            ->getCubeCollection($cubes)
+                            ->reject(fn ($count, $colour) => $this->hasTooManyForColour($colour, $count))
+                            ->isEmpty()
+                    ))
                     ->isEmpty()
             ))
             ->isEmpty();
+    }
+
+    private function getCubeCollection(string $cubes): Collection
+    {
+        return collect(array_combine(
+            Str::of($cubes)->matchAll('/(red|green|blue)/')->toArray(),
+            Str::of($cubes)->matchAll('/([0-9]+)/')->toArray(),
+        ));
+    }
+
+    private function findPowerOfFewestCubes(string $gameString): int
+    {
+        return intval(
+            Str::of($gameString)
+                ->explode('; ')
+                ->reduce(fn ($cubeCount, $cubes) => (
+                    Str::of($cubes)
+                        ->explode(', ')
+                        ->reduce(fn ($cubeCount, $cubes) => (
+                            $this
+                                ->getCubeCollection($cubes)
+                                ->reduce(fn ($cubeCount, $count, $colour) => (
+                                    $cubeCount->put($colour, max($cubeCount->get($colour), $count))
+                                ), $cubeCount)
+                        ), $cubeCount)
+                ), collect())
+                ->reduce(fn ($total, $count) => $total * $count, 1)
+        );
     }
 
     private function hasTooManyForColour(string $colour, string $count): bool
--- tests/Unit/Commands/DayTwoCommandTest.php
+++ tests/Unit/Commands/DayTwoCommandTest.php
@@ -27,11 +27,15 @@ it('solves part two of the puzzle', function () {
         ->makePartial()
         ->shouldReceive('getFileByLines')
         ->andReturn(collect([
-
+            'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+            'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+            'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+            'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+            'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
         ]))
         ->getMock();
 
     expect($mock->solvePuzzlePartTwo())
         ->toBeString()
-        ->toEqual('');
+        ->toEqual('2286');
 });
